4bb816553a9bfb932892b6cd10b19bafe39a6151
Eremor <artem.s13@gmail.com>
2020-03-17 14:53:20 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let morzeWord = [];
+    let morzeCode = [];
+    let morzeLetter = [];
+    let arr = '';
+    let decode = '';
+
+    for(let i = 0; i < expr.length; i += 10) {
+        morzeWord.push(expr.slice(i, i + 10));
+    }
+
+    morzeWord.forEach(item => {
+        for(let i = 0; i < item.length; i += 2) {
+            morzeCode.push(item.slice(i, i + 2));
+        }
+    });
+
+    for(let i = 0; i < morzeCode.length; i++) {
+        switch(morzeCode[i]) {
+            case '10':
+                morzeCode[i] = '.';
+                break;
+            case '11':
+                morzeCode[i] = '-';
+                break;
+            case '00':
+                morzeCode[i] = ' ';
+                break;
+            case '**':
+                morzeCode[i] = '*';
+                break
+        }
+        arr += morzeCode[i];
+    }
+
+    for(let i = 0; i < arr.length; i += 5) {
+        morzeLetter.push(arr.slice(i, i + 5).trim());
+    }
+
+    decode = morzeLetter.join(' ').split(' ***** ').map(a => a.split(' ').map(b => MORSE_TABLE[b]).join('')).join(' ');
+    return decode;
 }
 
 module.exports = {
